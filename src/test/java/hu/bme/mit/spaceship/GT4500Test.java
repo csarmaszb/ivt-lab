4c5879c5733ac7fdaad1e109d242a23285202ad6
Csarmasz Balázs <47641937+csarmaszb@users.noreply.github.com>
2020-05-21 01:03:40 +0200

    Code coverage all green in fireTorpedo

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -46,4 +46,135 @@ public class GT4500Test {
     verify(mockTP2, times(1)).fire(1);
   }
 
+  @Test
+  public void firetorpedo_Single_Alternate_Success() {
+    // Arrange
+    when(mockTP1.fire(1)).thenReturn(true);
+    when(mockTP2.fire(1)).thenReturn(true);
+
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+    ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTP1, times(1)).fire(1);
+    verify(mockTP2, times(1)).fire(1);
+  }
+
+  @Test
+  public void firetorpedo_Single_Next_Empty_Success() {
+    // Arrange
+    when(mockTP1.isEmpty()).thenReturn(true);
+    when(mockTP2.fire(1)).thenReturn(true);
+
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTP1, times(0)).fire(1);
+    verify(mockTP2, times(1)).fire(1);
+  }
+
+  @Test
+  public void firetorpedo_Single_First_Shots_Success() {
+    // Arrange
+    when(mockTP1.isEmpty()).thenReturn(true);
+    when(mockTP2.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+    verify(mockTP1, times(0)).fire(1);
+    verify(mockTP2, times(1)).fire(1);
+  }
+
+  @Test
+  public void firetorpedo_Single_AllStore_Empty_Success() {
+    // Arrange
+    when(mockTP1.isEmpty()).thenReturn(true);
+    when(mockTP2.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(false, result);
+    verify(mockTP1, times(0)).fire(1);
+    verify(mockTP2, times(0)).fire(1);
+  }
+
+  @Test
+  public void firetorpedo_All_AllStore_Empty_Success() {
+    // Arrange
+    when(mockTP1.isEmpty()).thenReturn(true);
+    when(mockTP2.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(false, result);
+    verify(mockTP1, times(0)).fire(1);
+    verify(mockTP2, times(0)).fire(1);
+  }
+
+  @Test
+  public void firetorpedo_Single_Primary_fired_Secondary_Empty_Success() {
+    // Arrange
+    when(mockTP1.fire(1)).thenReturn(true);
+    when(mockTP2.isEmpty()).thenReturn(true);
+    ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+    verify(mockTP1, times(2)).fire(1);
+    verify(mockTP2, times(0)).fire(1);
+  }
+
+  @Test
+  public void firetorpedo_None_Firingmode_Success() {
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.NONE);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void firetorpedo_Single_Primary_fired_Secondary_Empty_Primary_Empty_Success() {
+    // Arrange
+    when(mockTP1.fire(1)).thenReturn(true);
+    when(mockTP2.isEmpty()).thenReturn(true);
+    ship.fireTorpedo(FiringMode.SINGLE);
+    when(mockTP1.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(false, result);
+    verify(mockTP1, times(1)).fire(1);
+    verify(mockTP2, times(0)).fire(1);
+  }
+
+  @Test
+  public void fireTorpedo_All_Primary_Empty_Success(){
+    // Arrange
+    when(mockTP1.isEmpty()).thenReturn(true);
+    when(mockTP2.fire(1)).thenReturn(true);
+
+    // Act
+    ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    verify(mockTP1, times(0)).fire(1);
+    verify(mockTP2, times(1)).fire(1);
+  }
+
 }
